8fb5b2517a59a7979e89fe5d49cd676f50c01dc9
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-09-06 02:24:29 +0300

    last update to model

--- api01/models.py
+++ api01/models.py
@@ -2,8 +2,8 @@ from django.db import models
 # Create your models here.
 
 class Desha(models.Model):
-    title = models.CharField(max_lenght =200)
-    author = models.CharField(max_lenght =100)
+    title = models.CharField(max_length =200)
+    author = models.CharField(max_length =100)
     published_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
 
