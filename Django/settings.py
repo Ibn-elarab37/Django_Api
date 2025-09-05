e29ccafdbdc32c46af16bc0c4f242709bdf68b08
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-09-05 16:25:19 +0300

    update the allowed host = settings proj level

--- Django/settings.py
+++ Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!!6@g(_f!27mt+3=7a7mp-d1jt08g2=%do0x4u+n$m)s0i+bw+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
