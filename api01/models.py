e6036433b4e55520aa0c35b15d22a62a76231778
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-09-06 02:10:55 +0300

    models creation Books

--- api01/models.py
+++ api01/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+class Desha(models.Modle):
+    title = models.CharField(max_lenght =200)
+    author = models.CharField(max_lenght =100)
+    published_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
