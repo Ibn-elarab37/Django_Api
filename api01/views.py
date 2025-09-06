fcc82d8efe2c2eebc538ba67d5f6ae3ec69ead65
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-09-06 14:12:13 +0300

    update views app level

--- api01/views.py
+++ api01/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def api01(request):
-    return HttpResponse("Hello from API1")
+    return HttpResponse("Hello from API1"
+                        "Hello form the Other side \n Hello my dear friend form django ")
+
