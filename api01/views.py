1317a1c3b480e5145e744e3f1d8c6f02efc5359c
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-09-06 14:19:50 +0300

    fix deployment

--- api01/views.py
+++ api01/views.py
@@ -4,6 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def api01(request):
-    return HttpResponse("Hello from API1"
-                        "Hello form the Other side \n Hello my dear friend form django ")
+    return HttpResponse("Hello form the Other side \n Hello my dear friend form django ")
 
